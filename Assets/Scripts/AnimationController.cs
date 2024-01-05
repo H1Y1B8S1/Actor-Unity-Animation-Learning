252ea3d265ee13033e2c195bbe84a470122dec0f
H1Y1B8S1 <h1y1b8s1@gmail.com>
2024-01-05 14:09:21 +0530

    walk animation with bug

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -1,22 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class AnimationController : MonoBehaviour
 {
     public Animator characterAnimator;
-    private static readonly int JumpTrigger = Animator.StringToHash("JumpTrigger");
+    private static readonly int JumpTrigger = Animator.StringToHash("Jump");
+    private static readonly int WalkTrigger = Animator.StringToHash("Walk");
 
-    public void TriggerJumpAnimation()
+    //private static readonly int FallTrigger = Animator.StringToHash("Fall");
+    //private static readonly int RunTrigger = Animator.StringToHash("Run");
+    //private static readonly int SprintTrigger = Animator.StringToHash("Sprint");
+    //private static readonly int TalkTrigger = Animator.StringToHash("Talk");
+
+    public void JumpAnimation()
     {
         characterAnimator.SetTrigger(JumpTrigger);
     }
+    public void WalkAnimation()
+    {
+        characterAnimator.SetTrigger(WalkTrigger);
+    }
+
+    //public void FallAnimation()
+    //{
+    //    characterAnimator.SetTrigger(FallTrigger);
+    //}
+    //public void RunAnimation()
+    //{
+    //    characterAnimator.SetTrigger(RunTrigger);
+    //}
+    //public void SprintAnimation()
+    //{
+    //    characterAnimator.SetTrigger(SprintTrigger);
+    //}
+    //public void TalkAnimation()
+    //{
+    //    characterAnimator.SetTrigger(TalkTrigger);
+    //}
+
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
-        {
-            TriggerJumpAnimation(); // Call the existing method to trigger the jump animation
-        }
+            JumpAnimation(); // Call the existing method to trigger the jump animation
+        if (Input.GetKeyDown(KeyCode.W))
+            WalkAnimation();
+
+        //if (Input.GetKeyDown(KeyCode.E))
+        //    TalkAnimation();
+        //if (Input.GetKeyDown(KeyCode.LeftShift))
+        //    RunAnimation();
+        //if (Input.GetKeyDown(KeyCode.T))
+        //    SprintAnimation();
+        //if (Input.GetKeyDown(KeyCode.F))
+        //    FallAnimation();
     }
 }
