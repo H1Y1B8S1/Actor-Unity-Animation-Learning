5385b8199dc9e4295bbae51d131112ae3334658b
H1Y1B8S1 <h1y1b8s1@gmail.com>
2024-01-08 12:46:58 +0530

    code optimize

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -1,32 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class AnimationController : MonoBehaviour
 {
     public Animator characterAnimator;
-
+    private bool isWalking = false;
 
     public void JumpAnimation()
     {
-        //characterAnimator.SetTrigger(JumpTrigger);
+        // Trigger jump animation
+        characterAnimator.SetTrigger("JumpTrigger");
     }
-    public void WalkAnimation()
-    {
-        characterAnimator.SetBool("isWalking",true);
-    }
-
 
     void Update()
     {
         bool walk = Input.GetKey(KeyCode.W);
-        if (Input.GetKeyDown(KeyCode.Space))
-            JumpAnimation(); // Call the existing method to trigger the jump animation
-       if(walk)
-            characterAnimator.SetBool("isWalking",true);
-       if(!walk)
-            characterAnimator.SetBool("isWalking", false);
 
+        // Update isWalking flag only when necessary to avoid redundant calls
+        if (walk != isWalking)
+        {
+            isWalking = walk;
+            characterAnimator.SetBool("isWalking", isWalking);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            JumpAnimation(); // Trigger jump animation
+        }
     }
 }
